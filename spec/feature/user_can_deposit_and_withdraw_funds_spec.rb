9eefc62eeacb41e6cb1ec73c9aebf3b1fd5d5bec
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-27 16:29:14 +0000

    fix: re-add test that was accidentally deleted

--- spec/feature/user_can_deposit_and_withdraw_funds_spec.rb
+++ spec/feature/user_can_deposit_and_withdraw_funds_spec.rb
@@ -4,6 +4,10 @@ describe 'User can deposit and withdraw funds' do
 
   subject(:account) { Account.new }
 
+  it 'balance is 5 after depositing 5 in the account' do
+    expect(account.deposit(5)).to eq 5
+  end
+
   it 'balance is 1 after 10 is deposited and 9 is withdrawn' do
     account.deposit(10)
     expect(account.withdraw(9)).to eq 1
