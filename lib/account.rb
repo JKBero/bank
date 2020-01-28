7919665f9721f897f0d2344cb89674b3a369796a
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-28 12:00:15 +0000

    refactor: integrate Transaction class with deposit method and feature test

--- lib/account.rb
+++ lib/account.rb
@@ -8,8 +8,11 @@ class Account
     @statement = statement_class.new
   end
 
-  def deposit(amount)
+  def deposit(amount, transaction_class = Transaction)
     @balance += amount
+    transaction = transaction_class.new(balance: @balance, credit: amount)
+    @statement.add(transaction)
+    @balance
   end
 
   def withdraw(amount)
--- spec/feature/user_can_view_account_statement_spec.rb
+++ spec/feature/user_can_view_account_statement_spec.rb
@@ -14,4 +14,13 @@ describe 'User can view their account statement' do
     expect(account.statement).to eq result
   end
 
+  it 'shows one transaction after first deposit into account' do
+    allow(Date).to receive(:today) { Date.new(2019, 1, 27) }
+    account.deposit(100)
+    headers = "date || credit || debit || balance"
+    transaction = "27/01/2019 || 100.00 || || 100.00"
+
+    expect(account.statement).to eq headers + "\n" + transaction
+  end
+
 end
