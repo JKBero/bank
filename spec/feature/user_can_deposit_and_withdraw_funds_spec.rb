f81c9d0757713d283630ba7cb4dea8b067be1734
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-28 12:18:39 +0000

    tests: add feature tests for multiple lines on statement & many deposits/withdrawals

--- spec/feature/user_can_deposit_and_withdraw_funds_spec.rb
+++ spec/feature/user_can_deposit_and_withdraw_funds_spec.rb
@@ -4,13 +4,22 @@ describe 'User can deposit and withdraw funds' do
 
   subject(:account) { Account.new }
 
-  it 'balance is 5 after depositing 5 in the account' do
+  it 'returns a balance of 5 after depositing 5 in the account' do
     expect(account.deposit(5)).to eq 5
   end
 
-  it 'balance is 1 after 10 is deposited and 9 is withdrawn' do
+  it 'returns a balance of 1 after 10 is deposited and 9 is withdrawn' do
     account.deposit(10)
     expect(account.withdraw(9)).to eq 1
+  end
+
+  it 'returns a correct balance after several account transactions' do
+    account.deposit(1000.99)
+    account.withdraw(95)
+    account.withdraw(40)
+    account.deposit(90.50)
+    account.withdraw(20)
+    expect(account.withdraw(225)).to eq 711.49
   end
 
 end
--- spec/feature/user_can_view_account_statement_spec.rb
+++ spec/feature/user_can_view_account_statement_spec.rb
@@ -37,4 +37,26 @@ describe 'User can view their account statement' do
     expect(account.statement).to eq headers + "\n" + tran2 + "\n" + tran
   end
 
+  it 'shows multiple transactions after several deposits and withdrawals' do
+    allow(Date).to receive(:today) { Date.new(2019, 1, 27) }
+    account.deposit(100)
+
+    allow(Date).to receive(:today) { Date.new(2019, 1, 30) }
+    account.withdraw(50)
+
+    allow(Date).to receive(:today) { Date.new(2019, 2, 4) }
+    account.withdraw(50)
+
+    allow(Date).to receive(:today) { Date.new(2019, 2, 6) }
+    account.deposit(250)
+
+    headers = "date || credit || debit || balance"
+    tran = "27/01/2019 || 100.00 || || 100.00"
+    tran2 = "30/01/2019 || || 50.00 || 50.00"
+    tran3 = "04/02/2019 || || 50.00 || 0.00"
+    tran4 = "06/02/2019 || 250.00 || || 250.00"
+
+    expect(account.statement).to eq headers + "\n" + tran4 + "\n" + tran3 + "\n" + tran2 + "\n" + tran
+  end
+
 end
